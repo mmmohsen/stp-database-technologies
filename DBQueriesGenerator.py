ab0de8cf8c238b61d025037767619fd72be2e9af
Milena Malysheva <milenamalush@gmail.com>
2018-05-02 19:34:25 +0300

    csv loader problems

--- DBQueriesGenerator.py
+++ DBQueriesGenerator.py
@@ -1,18 +1,30 @@
-def create_table(table_name, dataset, connector):
+import os
+
+
+def create_table(dataset, connector):
     query = []
     for col in dataset.columns:
         query.append(col + " text")
-    connector.query("CREATE TABLE " + table_name + "(" + ",".join(query) + ");")
-
+    connector.query("CREATE TABLE " + os.environ['TABLENAME'], + "(" + ",".join(query) + ");")
+    i=0
     for index, row in dataset.iterrows():
+        if i>50:
+            break
         vals = []
         for item in row:
             vals.append("'" + str(item) + "'")
         vals = ",".join(vals)
-        connector.query("INSERT INTO " + table_name + " VALUES (" + vals + ");")
+        connector.query("INSERT INTO " + os.environ['TABLENAME'], + " VALUES (" + vals + ");")
     connector.commit()
 
 
-def table_exists(connector, table_str):
+def table_exists(connector):
     return bool(
-        connector.query("select exists(select relname from pg_class where relname='" + table_str + "')").fetchone()[0])
+        connector.query("select exists(select relname from pg_class where relname='" + os.environ['TABLENAME'] + "')").fetchone()[0])
+
+def add_index(connector, column_number):
+    connector.query("CREATE INDEX column"+column_number+"_index ON "+os.environ['TABLENAME']+"(column"+column_number+")")
+    connector.commit()
+
+# def drop_indexes(connector):
+#
--- main.py
+++ main.py
@@ -9,10 +9,12 @@ from PostgresConnector import PostgresConnector
 def main():
     connector = PostgresConnector()
     # print (connector.query('SELECT * FROM fortest;').fetchall())
-    if not table_exists(connector, os.environ['TABLENAME']):
-        df = pd.read_csv("lineitemSF1.csv", header=None, names=list(['column' + str(x + 1) for x in range(17)]))
-        create_table(os.environ['TABLENAME'], df, connector)
-
-
+    if not table_exists(connector):
+        chunksize = 100000
+        chunks = []
+        for chunk in pd.read_csv('lineitemSF1.csv', chunksize=chunksize, engine='c', low_memory=False, header=None, names=list(['column' + str(x) for x in range(17)])):
+            chunks.append(chunk)
+        df = pd.concat(chunks, axis=0)
+        create_table(df, connector)
 if __name__ == "__main__":
     main()
