b82b5b5012e82e73657c2e6414c06090fce5017c
Milena Malysheva <milenamalush@gmail.com>
2018-05-02 22:53:56 +0300

    execution time method, add index method, drop all indexes method

--- DBQueriesGenerator.py
+++ DBQueriesGenerator.py
@@ -5,26 +5,43 @@ def create_table(dataset, connector):
     query = []
     for col in dataset.columns:
         query.append(col + " text")
-    connector.query("CREATE TABLE " + os.environ['TABLENAME'], + "(" + ",".join(query) + ");")
-    i=0
+    connector.query("CREATE TABLE " + os.environ['TABLENAME'] + "(" + ",".join(query) + ");")
     for index, row in dataset.iterrows():
-        if i>50:
-            break
         vals = []
         for item in row:
             vals.append("'" + str(item) + "'")
         vals = ",".join(vals)
-        connector.query("INSERT INTO " + os.environ['TABLENAME'], + " VALUES (" + vals + ");")
+        connector.query("INSERT INTO " + os.environ['TABLENAME'] + " VALUES (" + vals + ");")
     connector.commit()
 
 
 def table_exists(connector):
     return bool(
-        connector.query("select exists(select relname from pg_class where relname='" + os.environ['TABLENAME'] + "')").fetchone()[0])
+        connector.query(
+            "select exists(select relname from pg_class where relname='" + os.environ['TABLENAME'] + "')").fetchone()[
+            0])
+
 
 def add_index(connector, column_number):
-    connector.query("CREATE INDEX column"+column_number+"_index ON "+os.environ['TABLENAME']+"(column"+column_number+")")
+    connector.query("CREATE INDEX column" + str(column_number) + "_index ON " + os.environ[
+        'TABLENAME'] + "(column" + str(column_number) + ")")
     connector.commit()
 
-# def drop_indexes(connector):
-#
+
+def get_execution_time(connector, query):
+    return explain_analyze_query(connector, query)[0][0]['Execution Time']
+
+
+def explain_analyze_query(connector, query):
+    return connector.query("EXPLAIN (ANALYZE true, FORMAT json)  " + query).fetchone()
+
+
+def drop_indexes(connector):
+    connector.query(connector.query("SELECT 'DROP INDEX ' || string_agg(indexrelid::regclass::text, ', ')"
+                    " FROM   pg_index  i"
+                    " LEFT   JOIN pg_depend d ON d.objid = i.indexrelid"
+                    " AND d.deptype = 'i'"
+                    " WHERE  i.indrelid = 'test_data'::regclass"
+                    " AND    d.objid IS NULL"
+                    ";").fetchone()[0])
+    connector.commit()
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 
 import pandas as pd
 
-from DBQueriesGenerator import create_table, table_exists
+from DBQueriesGenerator import create_table, table_exists, get_execution_time, add_index, drop_indexes
 from PostgresConnector import PostgresConnector
 
 
@@ -10,11 +10,12 @@ def main():
     connector = PostgresConnector()
     # print (connector.query('SELECT * FROM fortest;').fetchall())
     if not table_exists(connector):
-        chunksize = 100000
-        chunks = []
-        for chunk in pd.read_csv('lineitemSF1.csv', chunksize=chunksize, engine='c', low_memory=False, header=None, names=list(['column' + str(x) for x in range(17)])):
-            chunks.append(chunk)
-        df = pd.concat(chunks, axis=0)
+        df = pd.read_csv('lineitemSF1.csv', header=None, names=list(['column' + str(x) for x in range(17)]))
         create_table(df, connector)
+    query = "SELECT * from test_data where column3 = '1'"
+    print(get_execution_time(connector, query))
+    add_index(connector, 3)
+    print(get_execution_time(connector, query))
+    drop_indexes(connector)
 if __name__ == "__main__":
     main()
