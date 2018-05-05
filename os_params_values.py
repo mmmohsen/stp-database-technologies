89a633fa58fcd4683c7633e8a2472f3489373b37
Mahmoud Mohsen <mmohsen35@gmail.com>
2018-05-05 03:19:03 +0200

    Update os_params_values.py

--- os_params_values.py
+++ os_params_values.py
@@ -1,8 +1,8 @@
 import os
 
 os.environ['DBNAME'] = "bullshift"
-os.environ['PASSWORD'] = "postgres"
-os.environ['USER'] = "postgres"
-os.environ["FILENAME"] = "/Users/pegasus/Desktop/lineitem.csv"
+os.environ['PASSWORD'] = ""
+os.environ['USER'] = ""
+os.environ["FILENAME"] = ""
 os.environ['PORT']  = "5432"
 os.environ["TABLENAME"] = "lineitem"
