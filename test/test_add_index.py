c6d6198ad33a14773ca3c7573c2416f53b1a5906
Milena Malysheva <milenamalush@gmail.com>
2018-05-26 21:46:57 +0300

    get_estimated_execution_time function is added

--- test/test_add_index.py
+++ test/test_add_index.py
@@ -3,7 +3,7 @@ from unittest import TestCase
 
 from PostgresConnector import PostgresConnector
 from const import table_column_types, table_column_names
-from db import add_index, get_execution_time, drop_indexes
+from db import add_index, get_execution_time, drop_indexes, get_estimated_execution_time
 from main import queries_amount, table_name
 from queryPull import generate_query_pull
 
@@ -17,14 +17,11 @@ class TestAdd_index(TestCase):
         with open('../.query_pull', 'rb') as pickleFile:
             queries = pickle.load(pickleFile)
         for query in queries:
-            execution_time_before = 0
-            execution_time_after = 0
             indexes_to_add = [i[0] for i in (sorted(enumerate(query['sf_array']), key=lambda x: x[1]))[:3]]
-            for t in range(10):
-                execution_time_before += get_execution_time(connector, query['query'])
-                for index in indexes_to_add:
-                    add_index(connector, index, table_name)
-                execution_time_after += get_execution_time(connector, query['query'])
-                drop_indexes(connector, table_name)
-            print(execution_time_before/10, execution_time_after/10)
+            execution_time_before = get_estimated_execution_time(connector, query['query'])
+            for index in indexes_to_add:
+                add_index(connector, index, table_name)
+            execution_time_after = get_estimated_execution_time(connector, query['query'])
+            drop_indexes(connector, table_name)
+            print(execution_time_before, execution_time_after)
             # self.assertLess(execution_time_after, execution_time_before)
