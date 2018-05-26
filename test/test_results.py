5ef2df53af2e96882d879bee110526daa8de6969
Milena Malysheva <milenamalush@gmail.com>
2018-05-26 23:49:46 +0300

    changes for workload

--- test/test_results.py
+++ test/test_results.py
@@ -1,5 +1,6 @@
 from unittest import TestCase
 import pickle
+import numpy as np
 
 from PostgresConnector import PostgresConnector
 from const import table_column_types, table_column_names
@@ -10,9 +11,9 @@ from queryPull import generate_query_pull
 
 class TestResults(TestCase):
 
-    def __init__(self):
-        super().__init__()
-        self.__queries_amount = 10
+    def __init__(self, *args, **kwargs):
+        super(TestResults, self).__init__(*args, **kwargs)
+        self.__queries_amount = 5
         self.__index_amount = 3
 
     def test_results_for_two_columns(self):
@@ -30,20 +31,26 @@ class TestResults(TestCase):
     def __test_results(self, columns_participating):
         connector = PostgresConnector()
         drop_indexes(connector, table_name)
-        generate_query_pull('../.query_pull', self.__queries_amount, columns_participating, table_column_types,
+        generate_query_pull('../.test_query_pull_' + str(columns_participating), self.__queries_amount,
+                            columns_participating, table_column_types,
                             table_column_names,
                             table_name, connector)
-        with open('../.query_pull', 'rb') as pickleFile:
+        with open('../.test_query_pull_' + str(columns_participating), 'rb') as pickleFile:
             queries = pickle.load(pickleFile)
-        for query in queries:
-            indexes_to_add = [i[0] for i in (sorted(enumerate(query['sf_array']), key=lambda x: x[1]))[:self.__index_amount]]
-            for index in indexes_to_add:
-                add_index(connector, index, table_name)
-            heuristically_estimated_execution_time = get_estimated_execution_time(connector, query['query'])
-            drop_indexes(connector, table_name)
-            indexes_to_add = get_indexes(self.__index_amount, query)
-            for index in indexes_to_add:
-                add_index(connector, index, table_name)
-            model_estimated_execution_time = get_estimated_execution_time(connector, query['query'])
-            print(heuristically_estimated_execution_time, ' ', model_estimated_execution_time)
 
+        sf_array = np.array([query['sf_array'] for query in queries]).sum(axis=0)
+        indexes_to_add = [i[0] for i in
+                          (sorted(enumerate(sf_array), key=lambda x: x[1]))[:self.__index_amount]]
+        for index in indexes_to_add:
+            add_index(connector, index, table_name)
+        heuristically_estimated_execution_time = 0
+        for query in queries:
+            heuristically_estimated_execution_time += get_estimated_execution_time(connector, query['query'])
+        drop_indexes(connector, table_name)
+        indexes_to_add = get_indexes(self.__index_amount, queries)
+        for index in indexes_to_add:
+            add_index(connector, index, table_name)
+        model_estimated_execution_time = 0
+        for query in queries:
+            model_estimated_execution_time += get_estimated_execution_time(connector, query['query'])
+        print(heuristically_estimated_execution_time, ' ', model_estimated_execution_time)
