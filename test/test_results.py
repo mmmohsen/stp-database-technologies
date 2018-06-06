e70017c00bdaaa531e9cebb143b93d07ce70187b
Milena Malysheva <milenamalush@gmail.com>
2018-06-07 00:39:44 +0300

    t-test

--- test/test_results.py
+++ test/test_results.py
@@ -1,6 +1,8 @@
 import random
 from unittest import TestCase
 import numpy as np
+from scipy.stats import stats
+import itertools
 
 from PostgresConnector import PostgresConnector
 from const import table_column_types, table_column_names, COLUMNS_AMOUNT
@@ -31,9 +33,9 @@ class TestResults(TestCase):
 
     def __test_results(self, columns_participating):
 
-        def get_execution_time_for_indexes_configuration():
+        def get_execution_time_for_indexes_configuration(indexes):
             total_time = 0
-            for index in indexes_to_add:
+            for index in indexes:
                 add_index(connector, index, table_name)
                 total_time = 0
             for query in queries:
@@ -41,37 +43,50 @@ class TestResults(TestCase):
             drop_indexes(connector, table_name)
             return total_time
 
+        def add_execution_time_for_method_and_indexes_configuration(method, indexes):
+            if method in methods:
+                methods[method].append(get_execution_time_for_indexes_configuration(indexes))
+            else:
+                methods[method] = [get_execution_time_for_indexes_configuration(indexes)]
+
         connector = PostgresConnector()
         drop_indexes(connector, table_name)
-        queries = generate_query_pull('../.test_query_pull_' + str(columns_participating), self.__queries_amount,
-                            columns_participating, table_column_types,
-                            table_column_names,
-                            table_name, connector)
+        methods = {}
+        i = 0
+        np.warnings.filterwarnings('ignore')
+        while True:
+            queries = generate_query_pull('../.test_query_pull_' + str(columns_participating) + '_' + str(i),
+                                          self.__queries_amount,
+                                          columns_participating, table_column_types,
+                                          table_column_names,
+                                          table_name, connector)
+            i += 1
+            sf_array = np.array([query['sf_array'] for query in queries]).sum(axis=0)
 
-        sf_array = np.array([query['sf_array'] for query in queries]).sum(axis=0)
-        indexes_to_add = [i[0] for i in
-                          (sorted(enumerate(sf_array), key=lambda x: x[1]))[:self.__index_amount]]
-        heuristically_estimated_execution_time = get_execution_time_for_indexes_configuration()
-        drop_indexes(connector, table_name)
-        indexes_to_add = get_indexes_qagent(self.__index_amount, queries, True)
-        #extra clean up to make sure no indices left from the agent
-        qlearning_estimated_execution_time = get_execution_time_for_indexes_configuration()
-        drop_indexes(connector, table_name)
-        indexes_to_add = get_indexes_supervised(self.__index_amount, queries)
-        supervised_estimated_execution_time = get_execution_time_for_indexes_configuration()
-        drop_indexes(connector, table_name)
-        indexes_to_add = random.sample(range(COLUMNS_AMOUNT), self.__index_amount)
-        random_indexes_estimated_execution_time = get_execution_time_for_indexes_configuration()
-        drop_indexes(connector, table_name)
-        print('heuristic:{}, supervised:{}, random indexes:{}'.format(heuristically_estimated_execution_time,
-                                                                     supervised_estimated_execution_time,
-                                                                     random_indexes_estimated_execution_time))
+            indexes_to_add = [i[0] for i in
+                              (sorted(enumerate(sf_array), key=lambda x: x[1]))[:self.__index_amount]]
+            add_execution_time_for_method_and_indexes_configuration('heuristic', indexes_to_add)
 
-        print('heuristic:{}, qlearning:{}, random indexes:{}'.format(heuristically_estimated_execution_time,
-                                                                      qlearning_estimated_execution_time,
-                                                                      random_indexes_estimated_execution_time))
+            indexes_to_add = get_indexes_qagent(self.__index_amount, queries, True)
+            add_execution_time_for_method_and_indexes_configuration('qlearning', indexes_to_add)
+            #extra clean up to make sure no indices left from the agent
+            drop_indexes(connector, table_name)
 
-        self.assertGreater(heuristically_estimated_execution_time, supervised_estimated_execution_time)
-        self.assertGreater(random_indexes_estimated_execution_time, supervised_estimated_execution_time)
-        self.assertGreater(heuristically_estimated_execution_time, qlearning_estimated_execution_time)
-        self.assertGreater(random_indexes_estimated_execution_time, qlearning_estimated_execution_time)
+            indexes_to_add = get_indexes_supervised(self.__index_amount, queries)
+            add_execution_time_for_method_and_indexes_configuration('supervised', indexes_to_add)
+
+            indexes_to_add = random.sample(range(COLUMNS_AMOUNT), self.__index_amount)
+            add_execution_time_for_method_and_indexes_configuration('random', indexes_to_add)
+
+            times_combinations = list(itertools.combinations(methods.values(), 2))
+            p_values = [stats.ttest_ind(time[0], time[1])[1] for time in times_combinations]
+            print(p_values)
+            if all(p_value < 0.01 for p_value in p_values) and i >= 10 or i >= 50:
+                break
+            print('try #' + str(i))
+            for method, times in methods.items():
+                print('{}: {}'.format(method, np.mean(times)))
+        print('')
+
+        for method, times in methods.items():
+            print('{}: {}'.format(method, np.mean(times)))
