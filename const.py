f9584cb858a27da577fe1a23a7fc586ed0469300
Ali Hashaam <ali_1810@live.com>
2018-06-09 11:34:22 +0200

    tpc-h test

--- const.py
+++ const.py
@@ -3,3 +3,7 @@ table_column_types = ['integer', 'integer', 'integer', 'integer', 'integer', 'de
                       'char(1)', 'date', 'date', 'date', 'text', 'text', 'text', 'text']
 table_column_names = list(['column' + str(x) for x in range(COLUMNS_AMOUNT)])
 BATCH_SIZE = 5
+
+table_columns_names_1 = ["ORDERKEY", "PARTKEY", "SUPPKEY", "LINENUMBER", "QUANTITY", "EXTENDEDPRICE", "DISCOUNT", "TAX",
+                        "RETURNFLAG", "LINESTATUS", "SHIPDATE", "COMMITDATE", "RECEIPTDATE", "SHIPINSTRUCT",
+                       "SHIPMODE", "COMMENT", "COMMENT_1"]
